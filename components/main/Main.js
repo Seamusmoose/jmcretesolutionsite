f9ed77f2437219dffcb894de3df58a424b6d2ce2
Seamus O'Sullivan <info@seamuso.com>
2022-08-17 14:08:44 +0200

    contact aligned

--- components/main/Main.js
+++ components/main/Main.js
@@ -1,8 +1,6 @@
 import styles from "./main.module.css";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import phoneIcon from "../../public/phone.png";
-import emailIcon from "../../public/png-transparent-email-marketing-icon-letter-m-angle-text-triangle.png";
 import Character from "../../public/urban-line-builder-in-helmet-holds-drawing.png";
 import headerLogo from "../../public/white_logo_transparent_background.png";
 import Aos from "aos";
@@ -14,42 +12,48 @@ const Main = () => {
   }, []);
 
   return (
-    <div id="main" className={styles.container__layout}>
-      {/* <div data-aos="fade-right" className={styles.contact}>
-        <Image src={phoneIcon} height="30px" width="30px" layout="responsive" />
-      </div> */}
-
-      <div className={styles.image__container} data-aos="flip-left">
-        <Image
-          src={Character}
-          height={320}
-          width={190}
-          layout="intrinsic"
-          alt="Concrete-Character"
-          priority
-        />
-      </div>
-      <div className={styles.container__text}>
-        <Image
-          data-aos="fade-left"
-          src={headerLogo}
-          height={100}
-          width={350}
-          layout="intrinsic"
-          alt="header-logo"
-          priority
-        />
-        <p data-aos="fade-left">
-          We are a concreting business located in Indooroopilly servicing the
-          greater Brisbane area. We cover all areas from North Brisbane to Gold
-          Coast, from East Brisbane to Ipswich and all inbetween. We specialise
-          in all aspects of concreting and can help you with whatever job you
-          need. Including slabs, patios, footpaths, driveways, shed slabs,
-          steelfixing, formwork, commercial slabs and more. We provide
-          professional quotes and we are sure to leave you satisfied with the
-          job. Feel free to contact us anytime for a quote and we look forward
-          to hearing from you
-        </p>
+    <div className={styles.main__container}>
+      <div id="main"></div>
+      <div className={styles.container__layout}>
+        <div data-aos="fade-right" className={styles.contact}>
+          <a>Contact</a>
+          <div>
+            <a>Email: jmcretesolutions@gmail.com</a>
+            <a>Phone: 0422 293 113</a>
+          </div>
+        </div>
+        <div className={styles.image__container} data-aos="flip-left">
+          <Image
+            src={Character}
+            height={320}
+            width={190}
+            layout="intrinsic"
+            alt="Concrete-Character"
+            priority
+          />
+        </div>
+        <div className={styles.container__text}>
+          <Image
+            data-aos="fade-left"
+            src={headerLogo}
+            height={100}
+            width={350}
+            layout="intrinsic"
+            alt="header-logo"
+            priority
+          />
+          <p data-aos="fade-left">
+            We are a concreting business located in Indooroopilly servicing the
+            greater Brisbane area. We cover all areas from North Brisbane to
+            Gold Coast, from East Brisbane to Ipswich and all inbetween. We
+            specialise in all aspects of concreting and can help you with
+            whatever job you need. Including slabs, patios, footpaths,
+            driveways, shed slabs, steelfixing, formwork, commercial slabs and
+            more. We provide professional quotes and we are sure to leave you
+            satisfied with the job. Feel free to contact us anytime for a quote
+            and we look forward to hearing from you
+          </p>
+        </div>
       </div>
     </div>
   );
